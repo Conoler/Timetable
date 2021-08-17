8e499b728e8c0f024c20403a1ba507fc903f2d2c
s-ryo02 <baesr04@naver.com>
2021-08-17 12:51:34 +0900

    retry

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,7 +7,7 @@ public class Bot extends EventListener {
     public static void main(String[] args)
             throws Exception
     {
-        JDA jda = JDABuilder.createDefault("ODc1NzA4ODAxNzU4Njc0OTU1.YRZdYA.O8Pxg8q1oyoDaMrqmISFDVSZF44").addEventListeners(new TListener()).build();
+        JDA jda = JDABuilder.createDefault("ODc1NzA4ODAxNzU4Njc0OTU1.YRZdYA.xzae_5KoEP7JStCCzW4NICEy2eo").addEventListeners(new TListener()).build();
 
     }
 }
