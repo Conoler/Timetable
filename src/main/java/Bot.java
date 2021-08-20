64134653c17d680919a1bca28d90dbf3d8fcedf0
s-ryo02 <baesr04@naver.com>
2021-08-20 09:21:56 +0900

    re

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,7 +7,7 @@ public class Bot extends EventListener {
     public static void main(String[] args)
             throws Exception
     {
-        JDA jda = JDABuilder.createDefault("ODc1NzA4ODAxNzU4Njc0OTU1.YRZdYA.xzae_5KoEP7JStCCzW4NICEy2eo").addEventListeners(new TListener()).build();
+        JDA jda = JDABuilder.createDefault("ODc1NzA4ODAxNzU4Njc0OTU1.YRZdYA.Ts7-5fEJPlLvlu5BGYYOq9pGI0g").addEventListeners(new TListener()).build();
         jda.addEventListener(new ListenerManager());
         FileMake.FileMake();
         Data.getFile();
