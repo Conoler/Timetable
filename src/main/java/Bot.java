3c049fc931be34d5a61d2d46e145d9d0b9ba993d
Conoler <80590492+Conoler@users.noreply.github.com>
2021-08-20 11:29:53 +0900

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,7 +7,8 @@ public class Bot extends EventListener {
     public static void main(String[] args)
             throws Exception
     {
-        JDA jda = JDABuilder.createDefault(TOKEN).addEventListeners(new TListener()).build();
+        JDA jda = JDABuilder.createDefault(ODc1NzA4ODAxNzU4Njc0OTU1.YRZdYA.ykT3X
+-K0cC7GMlMsb31xKCtO2vE).addEventListeners(new TListener()).build();
         jda.addEventListener(new ListenerManager());
         FileMake.FileMake();
         Data.getFile();
